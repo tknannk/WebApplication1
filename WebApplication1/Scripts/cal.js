6fda20f2417df0460a360c645c6f57e33e236cda
Administrator <Administrator@V08>
2024-10-13 02:32:50 +0700

    Add project files.

---
+++ WebApplication1/Scripts/cal.js
@@ -0,0 +1,10 @@
+﻿function cong() {
+    var soA = parseInt(document.getElementById('soA').value);
+    var soB = parseInt(document.getElementById('soB').value);
+    var tinh = soA + soB;
+    document.getElementById('kq').innerHTML = tinh;
+}
+function tru() {
+
+}
+function tong()
